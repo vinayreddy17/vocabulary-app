606262df640a61ea9639c510f87380431d45d3e1
vinayreddy <vinaykumaramireddy@gmail.com>
2023-11-26 20:09:13 +0530

    second commit

--- controllers/wordcontroller.js
+++ controllers/wordcontroller.js
@@ -1,13 +1,66 @@
 
 
+// import { MongoClient } from 'mongodb';
+// // Connection URL
+// const url = 'mongodb://localhost:27017';
+
+// async function dbConnection() 
+// {
+//   const client = new MongoClient(url);
+//   const dbName = 'vocabulary';
+//   await client.connect();
+//   console.log("connected");
+//   return client;
+
+
+
+// }
+
+// const client = await dbConnection();
+
+
+
+
+// const getWords =(req,res)=>{
+//     const words=client.db(vocabulary).collection(words).find();
+//     res.send(words);
+//       // client.set(products);
+//       // res.status(200).send(products)
+//       // console.log("hello");
+//       // res.json({ message: "Words retrieved successfully" });
+
+//   }
+
+//  export {getWords};
 
 
 
 
 
-const getOneProduct = async(req,res)=>{
-    let id= req.params.id
-      let products = await Product.findone({where:{id:id}})
-      client.set(products);
-      res.status(200).send(products)
+// wordcontroller.js
+
+import { MongoClient } from 'mongodb';
+
+// const url = 'mongodb://localhost:27017';
+const url = 'mongodb://127.0.0.1:27017';
+const dbName = 'vocabulary';
+
+async function dbConnection() {
+  const client = new MongoClient(url, { useUnifiedTopology: true });
+  await client.connect();
+  console.log("Connected to MongoDB");
+  return client.db(dbName);
+}
+
+const getWords = async (req, res) => {
+  try {
+    const db = await dbConnection();
+    const words = await db.collection('words').find().toArray();
+    res.json(words);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
+};
+
+export { getWords };
--- server.js
+++ server.js
@@ -1,25 +1,23 @@
-const express = require('express');
+import express, { json } from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
-const { MongoClient } =require('mongodb')
-const Router = require('./Routes/words') 
+import cors from 'cors';
+// import bodyParser from 'body-parser';
+
+import { getWords } from "./controllers/wordcontroller.js";
 
 
-// mongodb connection
-const MONGO_URL = "mongodb://localhost:27017"
+const router =express.Router();
+app.use(json());
+app.use(cors());
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/api', router);
 
-async function dbConnection() {
-  const client = new MongoClient(MONGO_URL)
-  await client.connect()
-}
+app.get('/',(req,res)=>{
+  res.json({message:"hello "})
+})
+router.get('/getwords',getWords)
 
-
-
-app.use('/api/Words', Router);
-// Middleware to parse JSON requests
-app.use(express.json());
-
-// Define routes and API endpoints here
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
